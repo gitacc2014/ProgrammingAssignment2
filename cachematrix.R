40cd9dc005639b0bf7ef355d68312f3c2644aaab
gitacc2014 <learngit2014@gmail.com>
2015-01-25 14:08:54 -0500

    Matrix inverse cache 14:05PM

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	#Condition added to confine the matrices to square
+	if(is.matrix(x) && nrow(x)!=ncol(x))
+		{
+			print("Please enter square matrix only and retry")
+			return()
+		}
+		MatInv <- NULL
+		setMat <- function(y){
+			x <<-y
+			MatInv <<- NULL
+		}
+		getMat <- function() x
+		setMatInv <- function(inverse) MatInv <<- inverse
+		getMatInv <- function() MatInv
+    list(setMat=setMat,getMat=getMat,setMatInv=setMatInv,getMatInv=getMatInv)
 }
 
 
@@ -12,4 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        MatrixInverse <- x$getMatInv()
+        mat <- x$getMat()
+        if(!is.null(MatrixInverse))
+        {
+        	message("Getting cached inverse")
+        	return(MatrixInverse)
+        }
+        
+        MatrixInverse <- solve(mat)
+        x$setMatInv(MatrixInverse)
+        MatrixInverse
 }
